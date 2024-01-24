7aff16b7f14da788c8485b521561741a909f4d68
EvgenPletenskyi <e.pletenskyi@gmail.com>
2024-01-24 23:27:48 +0200

    anchor links

--- js/main.js
+++ js/main.js
@@ -44,3 +44,10 @@ $(document).ready(function(){
         ]
     });
 });
+
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
